1c848b11accf3ff6aa6a33c9a818f9617f2325e8
Bill Ray <bill.ray0259@gmail.com>
2023-04-02 15:02:35 -0400

    Updated cli to copy command to clipboard. Made prompt more concise.

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,7 @@
 import sys
 import openai
 import os
-
+import pyperclip
 
 API_KEY_FILE = os.path.expanduser("~/scripts/OPENAI_API_KEY.txt")
 
@@ -15,15 +15,28 @@ def translate_to_terminal_command(text):
         print("Please set your OpenAI API key in the OPENAI_API_KEY environment variable or in the OPENAI_API_KEY.txt file.")
         sys.exit(1)
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant that translates English text to terminal commands. Respond with only the terminal command and no other text."},
-            {"role": "user", "content": f"Translate the following English text to a terminal command: '{text}'"},
-        ]
-    )
-
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "Translate English text to terminal commands. Respond with only the terminal command and no other text."},
+                {"role": "user", "content": text},
+            ]
+        )
+    except Exception as e:
+        print("Error: ", e)
+        sys.exit(1)
+    
     return response.choices[0].message.content
+
+
+def process_command(command):
+    command = command.strip()
+    if command.startswith("```") and command.endswith("```"):
+        command = command[3:-3].strip()
+    if command.startswith("`") and command.endswith("`"):
+        command = command[1:-1].strip()
+    return command
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -31,5 +44,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     input_string = sys.argv[1]
-    translated_command = translate_to_terminal_command(input_string)
+    translated_command = translate_to_terminal_command(input_string).strip()
+    translated_command = process_command(translated_command)
     print(translated_command)
+    pyperclip.copy(translated_command)
